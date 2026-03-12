59e1accbcd0ac2462964556eb9872fc82088ff4b
RCDS <i.szegedi@aol.com>
2026-03-11 21:19:34 -0400

    Create SiteHeader.tsx

---
+++ app/components/SiteHeader.tsx
@@ -0,0 +1,15 @@
+export default function SiteHeader() {
+  return (
+    <header>
+      <nav>
+        <a href="/">Home</a> |{" "}
+        <a href="/create-career-passport">Create Your Career Passport</a> |{" "}
+        <a href="/pricing">Upgrade to Premium</a> |{" "}
+        <a href="/sign-in">Sign In</a> |{" "}
+        <a href="/partner/employers">Employers</a> |{" "}
+        <a href="/partner/nonprofits">Nonprofits</a> |{" "}
+        <a href="/contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
